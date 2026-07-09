d0c44ef84ab2fa647a286bcf0aab13289567c954
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-07-09 18:35:01 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,20 @@
+// How to create a variable in JavaScript
+
+let name = "Tapash"
+
+let age = 23;   
+
+//let value can be changed
+
+age = 25;
+
+console.log(name,age);
+
+
+const account = 1234;   //const value cannot be changed
+console.log(account);
+
+
+var a = 10;
+a = 20;
+console.log(a);
