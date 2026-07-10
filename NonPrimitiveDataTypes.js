bee360895efdf70debde4b54f8cc25d97a20ac51
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-07-10 18:36:12 +0530

    Add files via upload

---
+++ NonPrimitiveDataTypes.js
@@ -0,0 +1,28 @@
+// Non primitive data type 
+// array, object, function
+
+
+//array
+let arr = [10,20,11,"Rohit",true];
+console.log(arr);
+
+
+//object
+
+let user = {
+  name:"Tapash",
+  account: 12312,
+  age:23,
+  category: 'General'
+}
+
+return s = function add() {
+  console.log("Hello");
+}
+
+console.log(s());
+
+
+
+
+
---
+++ PrimitiveDataTypes.js
@@ -0,0 +1,82 @@
+// Primitive Data Types in JavaScript
+// number, string, boolean, null, undefined, symbol, bigint
+
+
+// number
+
+let a = 10;    
+let b = 2.36
+console.log(a,b);
+
+
+// string
+
+let c = "Tapash";
+let d = "Bharali";
+console.log(c,d);
+
+
+// Boolean
+
+let login = true;
+let f = false;
+console.log(login,f);
+
+
+// Undefined 
+
+let user;
+console.log(user);
+
+
+// Null
+
+let weather = null;
+console.log(weather);
+
+
+// Big int 
+
+let bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
+
+// symbol 
+
+const id1 = Symbol("id");
+console.log(id1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
