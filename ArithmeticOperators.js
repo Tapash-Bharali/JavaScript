27b7307e96ffaa9aa3c203f5c14e4e110366d230
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-07-10 19:07:20 +0530

    Add files via upload

---
+++ ArithmeticOperators.js
@@ -0,0 +1,6 @@
+console.log(5 + 3); // Addition
+console.log(5 - 3); // Subtraction
+console.log(5 * 3); // Multiplication
+console.log(5 / 3); // Division
+console.log(5 % 3); // Modulus
+console.log(5 ** 3); // Exponentiation
