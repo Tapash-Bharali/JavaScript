7d408b5edddaf14812be496bec5ffb501e3b60b7
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-07-11 09:35:45 +0530

    Add files via upload

---
+++ ComparisonOperators.js
@@ -0,0 +1,15 @@
+let x = 10;
+let y = 20;
+console.log(x > y); // false
+console.log(x < y); // true
+console.log(x >= y); // false
+console.log(x <= y); // true
+console.log(x == y); // false
+console.log(x != y); // true
+console.log(x === y); // false
+console.log(x !== y); // true
+
+
+
+
+
