22beff8e4c40dd3eea134ef991fb1b1f6aa46d12
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-07-13 10:38:17 +0530

    Add files via upload

---
+++ My_Array.js
@@ -0,0 +1,8 @@
+let marks = [100,50,70,90,80];
+console.log(marks);
+console.log(marks.length);
+console.log(marks[2]);
+console.log(typeof marks);
+marks[5] = 120;
+console.log(marks);
+console.log(marks.pop());
---
+++ Third_Array.js
@@ -0,0 +1,5 @@
+let arr1 = [10,27,9,95,43];
+let arr2 = arr1;
+
+arr2.push(60);
+console.log(arr1);
