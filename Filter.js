5c979a1488dd970befa43cac9eb53fb7373de94e
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-07-20 20:32:46 +0530

    Add files via upload

---
+++ Filter.js
@@ -0,0 +1,11 @@
+// filter() returns a new array with elements that pass the given test.
+
+const arr = [10,20,30,5,90,87];
+
+// true: select
+// false : not selected
+
+const newArr = arr.filter((number)=> number>25);
+
+console.log(newArr);
+
