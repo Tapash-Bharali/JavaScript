e6b0cec49aa43d138f8a4a81c1eaf1c0551c132a
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-07-20 22:26:45 +0530

    Add files via upload

---
+++ Map_Key.js
@@ -0,0 +1,12 @@
+const m1 = new Map([
+  ["Tapash", 23],
+  [2, "Tapash"],
+  [true,11],
+  [[10,30,11], "Vanshika"]
+]);
+
+
+m1.set({name: "Lola", age:20},false);
+console.log(m1);
+
+console.log(m1.get("Tapash"));
